c325635d2d7b580d2d8052c553e0ec36b4111dcb
Scott <david.bowen@interapthq.com>
2018-08-04 21:01:14 -0400

    solution - kata level 6kyu

---
+++ number_format.js
@@ -0,0 +1,5 @@
+var numberFormat = function (number) {
+    return (number + "").replace(/\b(\d+)((\.\d+)*)\b/g, function(a, b, c) {
+    return (b.charAt(0) > 0 && !(c || ".").lastIndexOf(".") ? b.replace(/(\d)(?=(\d{3})+$)/g, "$1,") : b) + c;
+  });
+};
